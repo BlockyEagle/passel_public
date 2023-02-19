0f52464bccec5d4f581e19b6d1bee8debb19608a
BlockyEagle <71843799+BlockyEagle@users.noreply.github.com>
2023-02-19 20:35:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,12 +43,12 @@ sendall = 0
 
 # TODO 
 # replace the 0 with the pins channel ID for your sever
-pins_channel = 948375476685111296/954794285222486046
+pins_channel = 748788755808387133
 
 # TODO
 # add any black listed channel IDs as a list separated by a comma (,)
 # a good idea is to add admin channels to this
-blacklisted_channels = []
+blacklisted_channels = [796313678416445480,672883464680243228,676497488399695893,1064134858944819260,817026269630955620,775298187652235284,673494970660945952,867532419480551454,947169436681527326,889906692047900723,734916654735163472,755751743488262175]
 
 # discord embed colors
 EMBED_COLORS = [
@@ -209,7 +209,8 @@ async def on_guild_channel_pins_update(channel, last_pin):
 
 
 # TODO Replace TOKEN with the token from discord developer portal 
-client.run('MTAwOTEyNjIzMTU4Njg5ODAyMg.GOPYKu.B3uxCiiU9IEOp3mWgoPe5Rq6b6w5Ynptc25Ews')
+client.run('MTA3NjkxNjE0NjY1MTAxNzMzNg.GLemMf.ygJUqvAjVO_UHr2ClqtIYg-Cl8VVijJP6YCIMk
+')
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
 #client.run(os.environ.get('TOKEN'))
